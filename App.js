9b5467b3126621b0bc7a214e76ba4c6aec16fda9
AAKASH1598 <47588233+AAKASH1598@users.noreply.github.com>
2020-09-24 22:51:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Navbar from './Navbar'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './Home'
+import Contact from './Contact';
+import Services from './Services'
+import Footer from './Footer';
+
 function App() {
   return (
     <>
     <Router>
     <Navbar/>
       <Switch>
-        <Route path='/' exact component={Home} />
+        <Route path='/home'  component={Home} />
+        <Route path='/Contact'  component={Contact} />
+        <Route path='/services'  component={Services} />
       </Switch>
+      <Footer/>
     </Router>
 
     </>
