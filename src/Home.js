ff4e325bec6af4c6d6c0596facca562a285ac2e4
AAKASH1598 <aakashdevajohn07@gmail.com>
2020-10-31 17:18:17 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,8 @@ import Introsite from './Introsite';
 import Qualities from './Qualities';
 import All_Products from './All_Products';
 import './Introsite.css'
-<<<<<<< HEAD
-
-=======
 import Download from './Download';
 import HomeAbout from './HomeAbout';
->>>>>>> 9dddab76252229c50fd38886d25cfbe2d2ba2db0
 
 function Home() {
   
