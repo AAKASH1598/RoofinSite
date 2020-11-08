4c377835eb00ff46593675af7b7b03938588878a
AAKASH1598 <aakashdevajohn07@gmail.com>
2020-11-08 13:54:53 +0530

    Why we are best

--- src/Qualities.js
+++ src/Qualities.js
@@ -1,62 +1,67 @@
 import React from 'react'
 import './Qualities.css'
 function Qualities() {
-    return (
-        <div className="header">
-        <h1>Why we are the Best?</h1>
-       
-        <div className="container_outer">
-         
-        <div className="container__main">
-            <div className="card">
-                <div className="circle">
-                    <h2><i class="fas fa-user-tie"></i></h2>
-
+return (
+ <div className="outer">     
+    <div className="container__flex">
+        <div className="card">
+                <div className="imgBox" data-text='Everon'>
+                    <h3><i class="fas fa-user-tie"></i></h3>
                 </div>
-                <div className="content">
+            <div className="content">
+                <div>
+                <h3>Everon</h3>
                 <p>“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” ―Aristotle</p>
                 </div>
             </div>
-            <div className="card">
-                <div className="circle">
-                    <h2><i class="fas fa-stopwatch"></i></h2>
-
+        </div>  
+        <div className="card">
+                <div className="imgBox" data-text='Time'>
+                    <h3><i class="fas fa-user-tie"></i></h3>
                 </div>
-                <div className="content">
-                <p>It is the time you have wasted for your rose that makes your rose so important. – Antoine de Saint-Exupéry, The Little Prince.</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="circle">
-                    <h2><i class="fas fa-hand-holding-usd"></i></h2>
-
-                </div>
-                <div className="content">
-                <p>Worth for the every single penny you spent. Invest on what that lasts Forever</p>
+            <div className="content">
+                <div>
+                <h3>Time</h3>
+                <p>“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” ―Aristotle</p>
                 </div>
             </div>
-            <div className="card">
-                <div className="circle">
-                    <h2><i class="fas fa-people-carry"></i></h2>
-
+        </div>   
+        <div className="card">
+                <div className="imgBox" data-text='Value for Money'>
+                    <h3><i class="fas fa-user-tie"></i></h3>
                 </div>
-                <div className="content">
-                <p>“Customer service shouldn't just be a department; it should be the entire company” – Tony Hsieh</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="circle">
-                    <h2><i class="fas fa-crown"></i></h2>
-
-                </div>
-                <div className="content">
-                <p>“Success usually comes to those who are too busy to be looking for it.” – Henry David Thoreau</p>
+            <div className="content">
+                <div>
+                <h3>Value for Money</h3>
+                <p>“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” ―Aristotle</p>
                 </div>
             </div>
-        </div>
-        </div>
-        </div>
-    )
+        </div>   
+        <div className="card">
+                <div className="imgBox" data-text='Customer Service'>
+                    <h3><i class="fas fa-user-tie"></i></h3>
+                </div>
+            <div className="content">
+                <div>
+                <h3>Customer Service</h3>
+                <p>“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” ―Aristotle</p>
+                </div>
+            </div>
+        </div>   
+        <div className="card">
+                <div className="imgBox" data-text='Achievements'>
+                     <h3><i class="fas fa-user-tie"></i></h3>
+                </div>
+            <div className="content">
+                <div>
+                <h3>Achievements</h3>
+                <p>“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” ―Aristotle</p>
+                </div>
+            </div>
+        </div>     
+    </div>
+</div>
+)
 }
 
 export default Qualities
